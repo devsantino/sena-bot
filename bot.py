b8fbeb7e34fd5b244df188f4eda109335d4879b2
santino <raoufcheg@gmail.com>
2025-03-19 00:51:17 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.message_content = True 
 intents.typing = False
 intents.presences = False
 intents.guilds = True
